d39f5a4a878178d498b557573d6ffdd1394683e5
Mickael Araujo <araujomickael18@gmail.com>
2020-01-23 02:35:44 -0300

    implementação inicial em app.js e estilização

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
+  
+  const [ user, setUser ] = useState('');
+  const [ users, setUsers ] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    setUser('');
+
+    loadUsers();
+  }
+
+  async function loadUsers() {
+    const response = await api.get(`/${user}`);
+    
+    const {  login, avatar_url, name, bio, repos_url } = response.data;
+
+    setUsers([...users, {
+      login,
+      avatar_url,
+      name,
+      bio,
+      repos_url
+    }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="userInput">
+          <label>Github Username:</label>
+          <input type="text" name="text" id="text" placeholder="your github username"
+          value={user} onChange={e => setUser(e.target.value)}
+          />
+        </div>
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {
+          users.map((item,index) => {
+            return (
+              <li key={index}>
+                <div className="info">
+                  <img src={item.avatar_url} alt={item.name}/>
+                  <h3>{item.name}</h3>
+                  <span>{item.login}</span>
+                  <p>{item.bio}</p>
+                  <a href={item.repos_url}>ver repositórios</a>
+                </div>
+              </li>
+              );
+            })
+        }
+      </ul>
+    </>
   );
 }
 
